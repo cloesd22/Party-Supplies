373fbdb400240f4ae8737546fd39442bbc50bba5
cloesd22 <cloesd22@gmail.com>
2017-09-29 00:30:44 +1000

    initial commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
+//Gulp auto deploy script.
+
 const gulp = require('gulp');
 const git = require('gulp-git');
 
 
 gulp.task('add', function(){
-    return gulp.src('.')
+    return gulp.src('./')
       .pipe(git.add()) 
       .pipe(git.commit(undefined, {
         args: '-m "initial commit"',
@@ -18,4 +20,19 @@ gulp.task('add', function(){
   });
 
 
+  gulp.task('pushHeroku', function(){
+    git.push('heroku', function (err) {
+      if (err) throw err;
+    });
+  });
+
   gulp.task('buildkit',['add','push']);
+
+  //Gulp auto deploy script.
+/*   "dependencies": {
+    "bootstrap": "^4.0.0-beta",
+    "gulp": "^3.9.1",
+    "webpack": "^3.5.5"
+  },
+  "devDependencies": {
+    "gulp-git": "^2.4.2" */
